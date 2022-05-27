1b624ea9a75f3e93b3c7ed7d0ac9cb39044b159c
atrettig <50344967+atrettig@users.noreply.github.com>
2022-05-27 13:04:49 -0400

    Add files via upload

--- movies1.js
+++ movies1.js
@@ -5,6 +5,24 @@ export const MOVIES = [
     ,"A Nightmare on Elm Street"
     ,"A Man Escaped"
     ,"A.I. Artificial Intelligence"
+    ,"Aladdin",
+    "Alien",
+    "Aliens",
+    "All That Jazz",
+    "All the President’s Men",
+    "Almost Famous",
+    "Amadeus",
+    "American Beauty",
+    "American Graffiti",
+    "American Psycho",
+    "Amélie",
+    "An American Werewolf in London",
+    "Annie Hall",
+    "Apocalypse Now",
+    "Army of Darkness",
+    "Back to the Future",
+    "Batman",
+    "Batman Begins" 
 
 
 ]
